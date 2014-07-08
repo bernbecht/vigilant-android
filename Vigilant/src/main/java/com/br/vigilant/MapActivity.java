356be05f88bb69d5750c3aceeca3651e07f8973f
Bernardo <berzuca@msn.com>
2014-07-08 11:03:11 +0100

    [01] Creating layout MAP Screen - Arrumar cor *buttons_bar* - 100%

--- Vigilant/src/main/java/com/br/vigilant/MapActivity.java
+++ Vigilant/src/main/java/com/br/vigilant/MapActivity.java
@@ -13,7 +13,7 @@ public class MapActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_map);
 
        mapInit();
 

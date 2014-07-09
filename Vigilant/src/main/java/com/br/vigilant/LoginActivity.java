713c23fdda554a197b74bf9b2a1beb3ae6b659ad
Bernardo <berzuca@msn.com>
2014-07-09 15:42:26 +0100

    [04] Create LOGIN screen -  70%

---
+++ Vigilant/src/main/java/com/br/vigilant/LoginActivity.java
@@ -0,0 +1,36 @@
+package com.br.vigilant;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import java.util.Map;
+
+/**
+ * Created by Berhell on 09/07/14.
+ */
+public class LoginActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+    }
+
+    public void facebookLogin(View view) {
+        Intent intent = new Intent(this, MapActivity.class);
+        startActivity(intent);
+    }
+
+    public void twitterLogin(View view) {
+        Intent intent = new Intent(this, MapActivity.class);
+        startActivity(intent);
+    }
+
+    public void googleLogin(View view) {
+        Intent intent = new Intent(this, MapActivity.class);
+        startActivity(intent);
+    }
+}

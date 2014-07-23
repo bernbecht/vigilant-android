8a15f405807e806e8222726d5d1d2205da20ba18
Bernardo <berzuca@msn.com>
2014-07-23 14:32:50 +0100

    Adding a report #10
    Tasks
    -------
    - [ ] Take picture;
    - [ ] Put the picture in a rectangle;
    - [x] Select a category on button Category
    - [x] Put the icons for the categories
    - [ ] character counter for **observation**

--- Vigilant/src/main/java/com/br/adapter/AdapterCategoriesList.java
+++ Vigilant/src/main/java/com/br/adapter/AdapterCategoriesList.java
@@ -1,10 +1,13 @@
 package com.br.adapter;
 
 import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.drawable.Drawable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.br.vigilant.R;
@@ -17,7 +20,7 @@ public class AdapterCategoriesList extends ArrayAdapter<String> {
     private final String[] values;
 
     public AdapterCategoriesList(Context context, String[] values) {
-        super(context, R.layout.adapter_settings_profile, values);
+        super(context, R.layout.adapter_categories, values);
         this.context = context;
         this.values = values;
     }
@@ -29,7 +32,15 @@ public class AdapterCategoriesList extends ArrayAdapter<String> {
         View rowView = inflater.inflate(R.layout.adapter_categories, parent, false);
 
         TextView textView = (TextView) rowView.findViewById(R.id.text_item_settings_profile);
-        textView.setText(values[position]);
+        ImageView image = (ImageView) rowView.findViewById(R.id.image_item_settings_profile);
+        String[] separated = values[position].split("-");
+        textView.setText(separated[0]);
+               Resources res = context.getResources();
+        int resID = res.getIdentifier(separated[1] , "drawable", context.getPackageName());
+        Drawable drawable = res.getDrawable(resID);
+        image.setImageDrawable(drawable);
+
+//        image.setImageResource(R.drawable.ic_airport);
 
         return rowView;
     }
--- Vigilant/src/main/java/com/br/vigilant/AddReportActivity.java
+++ Vigilant/src/main/java/com/br/vigilant/AddReportActivity.java
@@ -4,8 +4,13 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.res.Resources;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.br.adapter.AdapterCategoriesList;
 
@@ -32,12 +37,14 @@ public class AddReportActivity extends Activity {
         builderSingle.setIcon(R.drawable.ic_launcher);
         builderSingle.setTitle("Which category?");
 
-        String[] values1 = new String[]{"Airport", "Animals", "Environment and Pollution",
-        "Public Lighting", "Public Property", "Public Transport", "Road and Street Conditions",
-        "Trees", "Secutiry", "Rubbish", "Other", "Culture, Sports and Leisure", "Hospitals and Health Care",
-        "Family and Social Assistance", "Public Service"};
 
-        AdapterCategoriesList adapter1 = new AdapterCategoriesList(this, values1);
+
+        String[] categories_names = new String[]{"Airport-ic_airport", "Animals-ic_animals", "Environment and Pollution-ic_environment",
+        "Public Lighting-ic_lighting", "Public Property-ic_property", "Public Transport-ic_transport", "Road and Street Conditions-ic_road",
+        "Trees-ic_tree", "Secutiry-ic_security", "Rubbish-ic_rubbish", "Other-ic_others", "Culture, Sports and Leisure-ic_culture", "Hospitals and Health Care-ic_hospital",
+        "Family and Social Assistance-ic_family", "Public Service-ic_service"};
+
+        final AdapterCategoriesList adapter_categories = new AdapterCategoriesList(this, categories_names);
 
         builderSingle.setNegativeButton("cancel",
                 new DialogInterface.OnClickListener() {
@@ -48,11 +55,22 @@ public class AddReportActivity extends Activity {
                     }
                 });
 
-        builderSingle.setAdapter(adapter1,
+        builderSingle.setAdapter(adapter_categories,
                 new DialogInterface.OnClickListener() {
-
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
+                        String name = adapter_categories.getItem(which);
+                        String[] separated =name.split("-");
+                        Log.d("tag","click: "+name);
+
+                        TextView categoryName = (TextView) findViewById(R.id.add_report_category_name);
+                        categoryName.setText(separated[0]);
+
+                        ImageView categoryImage = (ImageView) findViewById(R.id.add_report_category_image);
+                        Resources res = context.getResources();
+                        int resID = res.getIdentifier(separated[1] , "drawable", context.getPackageName());
+                        Drawable drawable = res.getDrawable(resID);
+                        categoryImage.setImageDrawable(drawable);
                         dialog.dismiss();
                     }
                 });
